59c5900b497fbd5c3eb28b251dbaa34a0289810c
apark105 <andyjpark105@gmail.com>
2019-03-27 14:57:14 -0700

    added eslint and prettier

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,14 +1,14 @@
 import { sum, mul, sub, div } from '../src/sum';
 
-test('Adding 1 + 1 equals 2', () => {
-  expect(sum(1, 1)).toBe(2)
-})
+test('Adding 1 + 2 equals 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
 test('Multiplying 1 * 1 equals 1', () => {
-  expect(mul(1, 1)).toBe(1)
-})
+  expect(mul(1, 1)).toBe(1);
+});
 test('Subtracting 1 - 1 equals 0', () => {
-  expect(sub(1, 1)).toBe(0)
-})
+  expect(sub(1, 1)).toBe(0);
+});
 test('Dividing 1 / 1 equals 1', () => {
-  expect(div(1, 1)).toBe(1)
-})
+  expect(div(1, 1)).toBe(1);
+});
